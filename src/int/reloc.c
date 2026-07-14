5fa695474e4b11e57a8baaaa606c41c3faccc04f
sean <schobeck3@gmail.com>
2026-07-14 01:02:41 -0400

    small changes(*1)

--- src/int/reloc.c
+++ src/int/reloc.c
@@ -1,5 +1,18 @@
 /**
  * @author Sean Hobeck
- * @date 2026-07-09
+ * @date 2026-07-12
  */
 #include "reloc.h"
+
+/*! uses size_t. */
+#include <stddef.h>
+
+/*! uses internal. */
+#include "intt.h"
+
+#ifdef _
+
+internal
+allocate_near(void* target, size_t size) {
+
+}
